137b9757354580572c48b4b2e77e27b67f3d3438
ratlord <bennett.daniel@gmail.com>
2022-11-26 19:28:31 -0500

    intersection observer to for nav responses

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useMemo } from 'react'
 import './App.css'
 import styles from "./styles/App.module.scss"
 
@@ -15,6 +15,38 @@ function App() {
     section.current.scrollIntoView({behavior: "smooth"});
   }
 
+  const isInSection1 = useIsInViewport(section1);
+  console.log('isInViewport1: ', isInSection1);
+
+  const isInSection2 = useIsInViewport(section2);
+  console.log('isInViewport2: ', isInSection2);
+
+  const isInSection3 = useIsInViewport(section3);
+  console.log('isInViewport3: ', isInSection3);
+
+  function useIsInViewport(ref) {
+    const [isIntersecting, setIsIntersecting] = useState(false);
+  
+    const observer = useMemo(
+      () =>
+        new IntersectionObserver(([entry]) =>
+          setIsIntersecting(entry.isIntersecting),
+        ),
+      [],
+    );
+  
+    useEffect(() => {
+      observer.observe(ref.current);
+  
+      return () => {
+        observer.disconnect();
+      };
+    }, [ref, observer]);
+  
+    return isIntersecting;
+  }
+  
+
   return (
     <div>
       <FixedBg></FixedBg>
@@ -30,7 +62,8 @@ function App() {
           showArrow={true}
           />
       </div>
-
+    {/* section space, gives some leeway for intersection to observe only 1 section as when sections have 100vh both are identified as being in the viewport */}
+    <div className='sections-space'></div>
       <div ref={section2}>
         <Basic 
           image={`url(/src/assets/bg-2.jpg)`}
@@ -40,7 +73,8 @@ function App() {
           showArrow={true}
         />
       </div>
-
+      {/* section space, gives some leeway for intersection to observe only 1 section as when sections have 100vh both are identified as being in the viewport */}
+      <div className='sections-space'></div> 
       <div ref={section3}>
         <Basic 
           image={`url(/src/assets/bg-3.jpg)`}
@@ -53,5 +87,6 @@ function App() {
     </div>
   )
 }
+
 
 export default App
